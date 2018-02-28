7ee59faa84de834435e55e9b6b8330a777c1e147
Ani <ani.fullstack@gmail.com>
2018-02-27 19:45:48 -0700

    tuning enabled both android and ios

--- packages/mobile/.spinrc.js
+++ packages/mobile/.spinrc.js
@@ -1,52 +1,53 @@
-const url = require('url');
+const url = require("url");
 
 const config = {
   builders: {
     android: {
-      entry: './src/index.js',
-      buildDir: 'build/android',
-      dllBuildDir: 'build/android/dll',
-      stack: ['react-native', 'android'],
+      entry: "./src/index.js",
+      buildDir: "build/android",
+      dllBuildDir: "build/android/dll",
+      stack: ["react-native", "android"],
       defines: {
         __CLIENT__: true
       },
-      enabled: false
+      enabled: true
     },
     ios: {
-      entry: './src/index.js',
-      buildDir: 'build/ios',
-      dllBuildDir: 'build/ios/dll',
-      stack: ['react-native', 'ios'],
+      entry: "./src/index.js",
+      buildDir: "build/ios",
+      dllBuildDir: "build/ios/dll",
+      stack: ["react-native", "ios"],
       defines: {
         __CLIENT__: true
       },
-      enabled: false
+      enabled: true
     },
     test: {
-      stack: ['react-native-web', 'server'],
-      roles: ['test'],
+      stack: ["react-native-web", "server"],
+      roles: ["test"],
       defines: {
         __TEST__: true
       }
     }
   },
   options: {
-    stack: ['apollo', 'react', 'styled-components', 'es6', 'webpack'],
-    cache: '../../.cache',
+    stack: ["apollo", "react", "styled-components", "es6", "webpack"],
+    cache: "../../.cache",
     webpackDll: true,
     reactHotLoader: false,
     persistGraphQL: false,
     defines: {
-      __DEV__: process.env.NODE_ENV !== 'production',
+      __DEV__: process.env.NODE_ENV !== "production",
       __BACKEND_URL__: '"http://localhost:8080/graphql"'
     }
   }
 };
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   config.builders.android.enabled = true;
   config.builders.ios.enabled = true;
-  config.options.defines.__BACKEND_URL__ = '"https://apollo-universal-starter-kit.herokuapp.com/graphql"';
+  config.options.defines.__BACKEND_URL__ =
+    '"https://apollo-universal-starter-kit.herokuapp.com/graphql"';
   // Generating source maps for production will slowdown compilation for roughly 25%
   config.options.sourceMap = false;
 }
